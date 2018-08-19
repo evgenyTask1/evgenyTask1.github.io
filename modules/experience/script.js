372cd35f7bc18bf0641197886ee23191dccdc646
Evgeny T <official.trusov.mail@gmail.com>
2018-08-19 21:51:38 +0700

    Now corresponds to the template

--- modules/experience/script.js
+++ modules/experience/script.js
@@ -3,3 +3,5 @@ try {
     req = require.context("./img", true, /\.(?:png|jpe?g|gif|tiff)$/im);
     req.keys().forEach((key) => req(key)); 
 } catch(ex) {} // no img in this module
+
+import style from "./style.less"
